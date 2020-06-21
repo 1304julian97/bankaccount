8c7d7ed7de8a4ac2a7683654138fd03e9896c626
Julián Carvajal Montoya <julian@MacBook-Air-de-Julian.local>
2020-06-20 21:52:36 -0500

    Add domain services and File utils

--- src/main/scala/domainservices/DomainServices.scala
+++ src/main/scala/domainservices/DomainServices.scala
@@ -1,19 +1,39 @@
-package services
 
-import entities.{BanckAccount, SavingsAccount}
-import services.repository.Repository
+import entities.{ BanckAccount, SavingsAccount }
+import domainservices.repository.Repository
 
-
+import scala.util.{ Failure, Success, Try }
 
 class AccountServices {
 
-  def saveUpdateAccount(savingsAccountServices: SavingsAccount)(repository: Repository):Either[Throwable,String]={
-    Right("Hi!")
+  def saveUpdateAccount( savingsAccount: BanckAccount )( repository: Repository ): Either[Throwable, String] = {
+    val savingsAccountDAO = savingsAccount match {
+      case ba: SavingsAccount => MappersaDAO.convertSavingAccountEntityToDAO( ba )
+      case _                  => throw new Error( "paila" )
+
+    }
+    val `return` = repository match {
+      case sv: SavingAccountRepository => Try( sv.saveOrUpdate( savingsAccountDAO ) )
+    }
+
+    `return` match {
+      case Success( s ) => Right( "Account saved with success" )
+      case Failure( e ) => Left( e )
+
+    }
+
   }
 
-  def listAccount(repository: Repository):Either[Throwable,List[BanckAccount]]={
+  def getListAccount( repository: Repository ): Either[Throwable, List[BanckAccount]] = {
+    val acountsTry = repository match {
+      case sv: SavingAccountRepository => Try( sv.listAll() )
+    }
 
-    Right(List.empty)
+    acountsTry match {
+      case Success( la ) => Right( la.map( x => MappersaDAO.convertBanckAccountDAOToSavingAccoutEntity( x ) ) )
+      case Failure( e )  => Left( e )
+    }
   }
 
 }
+
