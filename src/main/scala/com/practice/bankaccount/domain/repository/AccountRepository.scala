fcffea6a91b3ce1a52307c61d2db97b31b5da480
Julián Carvajal Montoya <julian@MacBook-Air-de-Julian.local>
2020-06-29 20:49:37 -0500

    Refactor repository package

--- src/main/scala/com/practice/bankaccount/domain/repository/AccountRepository.scala
+++ src/main/scala/com/practice/bankaccount/domain/repository/AccountRepository.scala
@@ -1,5 +1,11 @@
 package com.practice.bankaccount.domain.repository
 
+import com.practice.bankaccount.domain.model.{ BankAccount }
+
 trait AccountRepository {
+
+  def upsert( account: BankAccount ): Either[String, BankAccount]
+
+  def list(): Either[String, List[BankAccount]]
 
 }
