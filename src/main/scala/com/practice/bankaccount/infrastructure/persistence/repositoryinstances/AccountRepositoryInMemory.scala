0e201c92f87325f319382e6fda1353e165d994a7
Julián Carvajal Montoya <julian@MacBook-Air-de-Julian.local>
2020-08-27 23:28:08 -0500

    Change data types

--- src/main/scala/com/practice/bankaccount/infrastructure/persistence/repositoryinstances/AccountRepositoryInMemory.scala
+++ src/main/scala/com/practice/bankaccount/infrastructure/persistence/repositoryinstances/AccountRepositoryInMemory.scala
@@ -1,19 +1,20 @@
-package com.practice.bankaccount.infrastructure.persistence.repository
+package com.practice.bankaccount.infrastructure.persistence.repositoryinstances
 
-import com.practice.bankaccount.domain.model.BankAccount
 import com.practice.bankaccount.domain.repository.AccountRepository
+import com.practice.bankaccount.infrastructure.persistence.dao.BankAccountDAO
+
 import scala.collection.mutable
 
 class AccountRepositoryInMemory extends AccountRepository {
 
-  private var accounts: mutable.Map[Int, BankAccount] = scala.collection.mutable.Map[Int, BankAccount]()
+  private var accounts: mutable.Map[Int, BankAccountDAO] = scala.collection.mutable.Map[Int, BankAccountDAO]()
 
-  def upsert( account: BankAccount ): Either[String, BankAccount] = {
-    accounts( account.number ) = account
+  def upsert( account: BankAccountDAO ): Either[String, BankAccountDAO] = {
+    accounts( account.accountNumber ) = account
     Right( account )
   }
 
-  def list(): Either[String, List[BankAccount]] = {
+  def list(): Either[String, List[BankAccountDAO]] = {
     Right( accounts.values.toList )
   }
 
