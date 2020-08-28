d8a6308e3e122f4b7d55852808aacbfb8f16638a
Julián Carvajal Montoya <julian@MacBook-Air-de-Julian.local>
2020-08-27 23:28:33 -0500

    Create H2 repository

--- src/main/scala/com/practice/bankaccount/infrastructure/persistence/repositoryinstances/H2AccountRepository.scala
+++ src/main/scala/com/practice/bankaccount/infrastructure/persistence/repositoryinstances/H2AccountRepository.scala
@@ -1,4 +1,19 @@
-package com.practice.bankaccount.infrastructure.persistence.repositoryinstances;
+package com.practice.bankaccount.infrastructure.persistence.repositoryinstances
 
-public class H2AccountRepository {
+import com.practice.bankaccount.domain.repository.AccountRepository
+import com.practice.bankaccount.infrastructure.persistence.dao.BankAccountDAO;
+
+class H2AccountRepository extends AccountRepository {
+  val port: Int = 8080
+  val host: String = "localhost"
+  val user: String = "DummyUser"
+  val password: String = "DummyPassword"
+
+  def upsert( account: BankAccountDAO ): Right[String, BankAccountDAO] = {
+    Right( account )
+  }
+
+  def list(): Either[String, List[BankAccountDAO]] = {
+    Left( "Something was wrong" )
+  }
 }
