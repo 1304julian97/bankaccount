4b3ae8a498f4f871ad4ef5be8641cf5a99091ef9
km3ll <kmell@protonmail.com>
2020-09-13 22:07:02 -0500

    Define domain entities

--- src/main/scala/com/practice/bankaccount/domain/model/BankAccount.scala
+++ src/main/scala/com/practice/bankaccount/domain/model/BankAccount.scala
@@ -2,39 +2,48 @@ package com.practice.bankaccount.domain.model
 
 import java.time.LocalDateTime
 
-import com.practice.bankaccount.domain.model.AccountStatus.AccountStatus
-
 sealed trait BankAccount {
   val number: Int
   val openDate: LocalDateTime
-  val status: AccountStatus
+  val status: Status
   val balance: Int
 }
 
-case class SavingsAccount( number: Int, openDate: LocalDateTime, status: AccountStatus, balance: Int ) extends BankAccount
+case class SavingsAccount( number: Int, openDate: LocalDateTime, status: Status, balance: Int, rate: Double ) extends BankAccount
+case class CheckingAccount( number: Int, openDate: LocalDateTime, status: Status, balance: Int ) extends BankAccount
 
-case class CheckingAccount( number: Int, openDate: LocalDateTime, status: AccountStatus, balance: Int ) extends BankAccount
 
 object BankAccount {
 
-  def createSavingsAccount( number: Int, balance: Int ): Either[String, SavingsAccount] = {
-    if ( number <= 0 ) {
-      Left( "Account number must be greater than 0" )
-    } else if ( balance < 0 ) {
-      Left( "Balance must not be negative" )
-    } else {
-      Right( SavingsAccount( number, LocalDateTime.now, AccountStatus.ACTIVE, balance ) )
-    }
+  private def validateNumber( number: Int ): Either[String, Int] = {
+    if ( number <= 0 ) Left( s"Account number must be greater than 0. Received '$number'" )
+    else Right( number )
+  }
+
+  private def validateBalance( balance: Int ): Either[String, Int] = {
+    if ( balance < 0 ) Left( s"Balance must not be negative. Received '$balance'" )
+    else Right( balance )
+  }
+
+  private def validateRate( rate: Double ): Either[String, Double] = {
+    if ( rate < 0.1 && rate > 2.3) Left( s"Rate of interest must be between 0.1% and 2.3%. Received '$rate'" )
+    else Right( rate )
+  }
+
+
+  def createSavingsAccount( number: Int, balance: Int, rate: Double ): Either[String, SavingsAccount] = {
+    for {
+      validNumber <- validateNumber( number )
+      validBalance <- validateBalance( balance )
+      validRate <- validateRate( rate )
+    } yield SavingsAccount( validNumber, LocalDateTime.now, ACTIVE, validBalance, validRate )
   }
 
   def createCheckingAccount( number: Int, balance: Int ): Either[String, CheckingAccount] = {
-    if ( number <= 0 ) {
-      Left( "Account number must be greater than 0" )
-    } else if ( balance < 0 ) {
-      Left( "Balance must not be negative" )
-    } else {
-      Right( CheckingAccount( number, LocalDateTime.now, AccountStatus.ACTIVE, balance ) )
-    }
+    for {
+      validNumber <- validateNumber( number )
+      validBalance <- validateBalance( balance )
+    } yield CheckingAccount( validNumber, LocalDateTime.now, ACTIVE, validBalance )
   }
 
 }
