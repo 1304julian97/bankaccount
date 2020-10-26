8a6a2ba0aa3998a59c305279ea4b7c399d067b63
Julián Carvajal <julian@MacBook-Air-de-Julian.local>
2020-10-25 21:07:45 -0500

    Add Route and response

--- src/main/scala/com/practice/bankaccount/application/adapters/http/Routes.scala
+++ src/main/scala/com/practice/bankaccount/application/adapters/http/Routes.scala
@@ -3,19 +3,20 @@ package com.practice.bankaccount.application.adapters.http
 import java.time.ZonedDateTime
 import java.util.UUID
 
-import akka.http.scaladsl.model.StatusCodes.{ InternalServerError, OK, RequestTimeout }
+import akka.http.scaladsl.model.StatusCodes.{InternalServerError, OK, RequestTimeout}
 import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.server.Directives.{ get }
-import akka.http.scaladsl.server.{ Route }
-import com.practice.bankaccount.application.cqrs.{ CommanOpenAccount, QueryGetAccounts }
+import akka.http.scaladsl.server.Directives.get
+import akka.http.scaladsl.server.Route
+import com.practice.bankaccount.application.cqrs.{CommanOpenAccount, QueryFilterAccount, QueryGetAccounts}
 import com.practice.bankaccount.application.dto.ApplicationDto._
 import com.practice.bankaccount.application.dto._
 import com.practice.bankaccount.application.main.Context
 import com.practice.bankaccount.domain.model.BankAccount
 import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport._
 import org.slf4j.LoggerFactory
-import scala.concurrent.{ Future }
-import scala.util.{ Failure, Success }
+
+import scala.concurrent.Future
+import scala.util.{Failure, Success}
 
 trait Routes extends MappersDto with JsonDecoders {
 
@@ -37,7 +38,24 @@ trait Routes extends MappersDto with JsonDecoders {
             complete( RequestTimeout -> ErrorResponse( now.toString, "Something was wrong" ) )
         }
 
-      } ~
+      } ~ get{
+        //bank-accounts/qtyLimit={someNumber}
+        headerValueByName("token"){ token =>
+          parameter("qtyLimit"){ qtyLimit =>
+            val result:Future[Either[String,List[BankAccount]]] = QueryFilterAccount.execute(qtyLimit.toInt)(context)
+            onComplete(result) {
+              case Success(Right(list)) =>  complete( OK -> GetAccountsResponse( now.toString, list.map( mapBankAccountToDTO ) ) )
+              case Success(Left(errorMessage)) => complete( InternalServerError -> ErrorResponse( now.toString, errorMessage ) )
+              case Failure( ex ) =>
+                val messageId: String = UUID.randomUUID().toString
+                LoggerFactory.getLogger( "Routes.class" ).error( s"Something was wrong getting accounts with balance greater than $qtyLimit, Use this code: $messageId", ex )
+                complete( RequestTimeout -> ErrorResponse( now.toString, "Something was wrong" ) )
+            }
+            }
+          }
+        }
+
+      }~
         post {
           entity( as[OpenAccountRequest] ) { request =>
             val result = CommanOpenAccount.execute( request.number, request.balance, request.accountType )( context )
