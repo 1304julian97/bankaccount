7ee1ee683ed17a8372e77fdfcb61ad756a8c5c51
Julián Carvajal <julian@MacBook-Air-de-Julian.local>
2020-10-25 21:06:48 -0500

    Add method

--- src/main/scala/com/practice/bankaccount/domain/service/AccountService.scala
+++ src/main/scala/com/practice/bankaccount/domain/service/AccountService.scala
@@ -1,10 +1,13 @@
 package com.practice.bankaccount.domain.service
 
+import cats.data.EitherT
+import cats.implicits._
 import com.practice.bankaccount.domain.model.BankAccount
 import com.practice.bankaccount.domain.repository.AccountRepository
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
+import scala.util.Success
 
 object AccountService {
 
@@ -37,5 +40,17 @@ object AccountService {
   def listAcounts()( repository: AccountRepository ): Future[Either[String, List[BankAccount]]] = {
     repository.list()
   }
+
+  def filterAccountByBalanceLimit(balanceLimit: Int)(repository: AccountRepository): Future[Either[String, List[BankAccount]]] = {
+
+    val accountsFiltered = for {
+      accounts <- EitherT(listAcounts()(repository))
+      filter <- EitherT.rightT(accounts.filter(_.balance <= balanceLimit))
+    } yield filter
+    accountsFiltered.value
+
+
+  }
+
 
 }
